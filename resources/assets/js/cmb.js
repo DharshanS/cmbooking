9c360f9d2b6d11898d4a47bd08e1bdddda770ebe
Dharshan <dharshan@Dharshans-MacBook-Pro.local>
2018-05-31 18:29:28 +0530

    add vendor files 2

--- resources/assets/js/cmb.js
+++ resources/assets/js/cmb.js
@@ -2,6 +2,32 @@
  * Created by dharshan on 5/31/18.
  */
 $(document).ready(function(){
+    var availableTags = [
+        "ActionScript",
+        "AppleScript",
+        "Asp",
+        "BASIC",
+        "C",
+        "C++",
+        "Clojure",
+        "COBOL",
+        "ColdFusion",
+        "Erlang",
+        "Fortran",
+        "Groovy",
+        "Haskell",
+        "Java",
+        "JavaScript",
+        "Lisp",
+        "Perl",
+        "PHP",
+        "Python",
+        "Ruby",
+        "Scala",
+        "Scheme"
+    ];
+    $( "#tags" ).autocomplete({
+        source: availableTags
+    });
 
-    alert("file load");
 });
